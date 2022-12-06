0da068ae386a85f75ae61518988b9d1f92e1a97f
Abdullah <abdullahsheikh207@gmail.com>
2022-12-06 23:30:02 +0500

    second commit

--- app/src/main/java/com/example/quranassignment/MainActivity.java
+++ app/src/main/java/com/example/quranassignment/MainActivity.java
@@ -27,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     Button search;
-    EditText surat;
-    EditText ayat;
+    EditText suratNum;
+    EditText ayatNum;
     TextView disp;
 
 
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         search = findViewById(R.id.btnSearch);
-        surat = findViewById(R.id.surahNum);
-        ayat = findViewById(R.id.ayatNum);
+        suratNum = findViewById(R.id.surahNum);
+        ayatNum = findViewById(R.id.ayatNum);
         disp = findViewById(R.id.disView);
         quranSurahHelper qsh = new quranSurahHelper();
         quranArabicTxt qt = new quranArabicTxt();
@@ -50,11 +50,25 @@ public class MainActivity extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int suratInt = Integer.parseInt(surat.getText().toString());
-                int ayatInt = Integer.parseInt((ayat.getText().toString()));
-                int temp1 = qsh.SSP[suratInt-1];
-                int temp2 = temp1 + ayatInt;
-                disp.setText(qt.QuranArabicText[temp2-1]);
+                int suratInt = Integer.parseInt(suratNum.getText().toString());
+                int ayatInt = Integer.parseInt((ayatNum.getText().toString()));
+                if(suratInt>0 && ayatInt >0) {
+                    int temp1 = qsh.SSP[suratInt - 1];
+                    int validAyatNo = qsh.SSP[suratInt]-qsh.SSP[suratInt-1]-1;
+                    System.out.println(validAyatNo);
+                    int temp2 = temp1 + ayatInt;
+                    if(ayatInt<=validAyatNo) {
+                        disp.setText(qt.QuranArabicText[temp2 - 1]);
+                    }
+                    else {
+                        disp.setText(("Not a Valid Ayat Number. "
+                                        + "This surah has "+ validAyatNo+ " ayats."));
+                    }
+                }
+                else{
+                    disp.setText("Please Enter Valid Surah and Ayat Number." +
+                                "Quran Has 114 surats");
+                }
 
             }
         });
